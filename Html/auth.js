044cb618471948fd20540768aac1afae9f462e20
marXus-3D <98558807+marXus-3D@users.noreply.github.com>
2024-07-26 17:43:44 +0300

    login without form validation

--- Html/auth.js
+++ Html/auth.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js"
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- Html/login.js
+++ Html/login.js
@@ -1,12 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYfBm1bVX-jWuGY1ThG3FgGU3V_QLZB7Q",
+  authDomain: "altium-cd024.firebaseapp.com",
+  projectId: "altium-cd024",
+  storageBucket: "altium-cd024.appspot.com",
+  messagingSenderId: "904813905259",
+  appId: "1:904813905259:web:f1bb555fdb05b18022382e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
 const signUpBtn = document.querySelector(".nextBtn");
 
+var logged = false;
 let email;
+let password;
 signUpBtn.addEventListener("click", (e) => {
   e.preventDefault();
   email = document.querySelector(".email").value;
-  const password = document.querySelector(".password").value;
+  password = document.querySelector(".password").value;
 
-  fetch(`http://localhost:8000/api/v1/altium/users/${email}`)
+  if (password) {
+    signIn();
+  }
+
+  if (!logged) {
+    fetch(`http://localhost:8000/api/v1/altium/users/${email}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -22,11 +51,34 @@ signUpBtn.addEventListener("click", (e) => {
         document.querySelector(".password").style.display = "block";
         document.querySelector(".password").classList.remove("hidePass");
         signUpBtn.innerText = "Login";
+        logged = true;
       } else {
         console.error("Unexpected response structure:", data);
+        alert("Username of Email doesn't exist");
       }
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
+      alert("Username of Email doesn't exist");
     });
+  }
 });
+
+function signIn() {
+  alert("Logged In");
+  signInWithEmailAndPassword(auth, email, password)
+      .then((userCred) => {
+          console.log(`User logged in ${userCred.user}`);
+          localStorage.setItem('loggedInUserId', userCred.user.uid);
+          window.location.href="";
+      })
+      .catch((err)=>{
+          console.log(err);
+          if(err.code == 'auth/invalid-credential'){
+              console.log('Incorrect Email and Password Combination');
+          }
+          else{
+              console.log('Account does not Exist');
+          }
+      });
+}
