0eba66c7ecda21b9039e46f46c8d11802fe99113
marXus-3D <98558807+marXus-3D@users.noreply.github.com>
2024-08-27 17:07:43 +0300

    working sign in

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // app.use(cors({ origin: 'http://localhost:3000' }));
 // app.use(cors());
 app.use(cors({
-    origin: ['http://localhost:3000/', 'https://altium-theta.vercel.app/']
+    origin: ['http://localhost:3000', 'https://altium-theta.vercel.app']
   }));
 app.use(express.json());
 
